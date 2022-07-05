3ede95e74acce7fdf71609910685c9d0ced94973
jacie__ <nana.mboussi@gmail.com>
2022-07-05 18:28:43 +0200

    STA Done

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Data] (
+    [CallTimestamp] nvarchar(50),
+    [CallType] nvarchar(50),
+    [EmployeeID] nvarchar(50),
+    [CallDuration] nvarchar(50),
+    [WaitTime] nvarchar(50),
+    [CallAbandoned] nvarchar(50)
+)
+
+SELECT *
+from Data
